c2b204ab1af4f3ff0a87cb432c4723aef1a491c5
yakovdonde <yakovdonde@gmail.com>
2022-12-27 16:15:20 +0400

    .

--- first_script.sh
+++ first_script.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+###This script prints system info ###
+
+
+echo "Welcome to bash script"
+echo
+
+###Checking system uptime###
+echo "####################"
+echo "The uptime of the system is:"
+uptime
+echo
+### Checking memory utilization ###
+echo "####################"
+echo  "Memory Utilization"
+free -h
+echo
+
+### Checking disk utilization ###
+echo "####################"
+echo "Disk Utilization"
+df -h
